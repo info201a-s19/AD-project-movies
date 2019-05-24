7ad43b44e713374ecc694436fea8aa56feb083f0
Jody Wong <jodyw22@uw.edu>
2019-05-24 16:29:40 -0700

    Edited the summary_info.R file so that it's filled with information regarding the summary of the movies_metadata.csv file

--- scripts/summary_info.R
+++ scripts/summary_info.R
@@ -10,41 +10,45 @@ library(dplyr)
 # Calculate the highest vote count's movie
 # Calculate the tagline of the highest vote  count
 # Caclulate the top 3 highest revenue's titles
-
-movies_metadata <- read.csv("data/movies_metadata.csv",
-                            stringsAsFactors = FALSE)
-
 # A function that takes in a dataset and returns a list of info about it:
 get_summary_info <- function(dataset) {
   ret <- list()
-  ret$longest_runtime <- dataset %>%
+  
+  longest_runtime <- dataset %>%
     select(runtime, title) %>%
     arrange(-runtime) %>%
     top_n(1, runtime) %>%
     pull(runtime)
-  ret$name_longest_runtime <- dataset %>%
+  
+  name_longest_runtime <- dataset %>%
     select(runtime, title) %>%
     arrange(-runtime) %>%
     top_n(1, runtime) %>%
     pull(title)
-  ret$highest_vote_count <- dataset %>%
+  
+  highest_vote_count <- dataset %>%
     select(vote_count, title, tagline) %>%
     arrange(-vote_count) %>%
     top_n(1, vote_count) %>%
     pull(title)
-  ret$highest_vote_count_tagline <- dataset %>%
+  
+  highest_vote_count_tagline <- dataset %>%
     select(vote_count, title, tagline) %>%
     arrange(-vote_count) %>%
     top_n(1, vote_count) %>%
     pull(tagline)
-  ret$top_3_revnue_titles <- dataset %>%
+  
+  top_3_revnue_titles <- dataset %>%
     select(revenue, title) %>%
     arrange(-revenue) %>%
     top_n(3, revenue) %>%
     pull(title)
+  
+  ret$longest_runtime <- longest_runtime
+  ret$name_longest_runtime <- name_longest_runtime
+  ret$highest_vote_count <- highest_vote_count
+  ret$highest_vote_count_tagline <- highest_vote_count_tagline
+  ret$top_3_revenue_titles <- top_3_revenue_titles
+  
   return (ret)
 }
-
-# Demonstrate that your function works by passing the dataset into it
-# and saving the result in a variable
-summary_metadata <- get_summary_info(movies_metadata)
