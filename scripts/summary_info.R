68c26c8dc1c982f8e93f7a5cf1139bc152379dc1
Jody Wong <jodyw22@uw.edu>
2019-05-24 16:37:45 -0700

    Edited summary_info.R so there's no erros (within lintr).

--- scripts/summary_info.R
+++ scripts/summary_info.R
@@ -13,42 +13,35 @@ library(dplyr)
 # A function that takes in a dataset and returns a list of info about it:
 get_summary_info <- function(dataset) {
   ret <- list()
-  
   longest_runtime <- dataset %>%
     select(runtime, title) %>%
     arrange(-runtime) %>%
     top_n(1, runtime) %>%
     pull(runtime)
-  
   name_longest_runtime <- dataset %>%
     select(runtime, title) %>%
     arrange(-runtime) %>%
     top_n(1, runtime) %>%
     pull(title)
-  
   highest_vote_count <- dataset %>%
     select(vote_count, title, tagline) %>%
     arrange(-vote_count) %>%
     top_n(1, vote_count) %>%
     pull(title)
-  
   highest_vote_count_tagline <- dataset %>%
     select(vote_count, title, tagline) %>%
     arrange(-vote_count) %>%
     top_n(1, vote_count) %>%
     pull(tagline)
-  
-  top_3_revnue_titles <- dataset %>%
+  top_3_revenue_titles <- dataset %>%
     select(revenue, title) %>%
     arrange(-revenue) %>%
     top_n(3, revenue) %>%
     pull(title)
-  
   ret$longest_runtime <- longest_runtime
   ret$name_longest_runtime <- name_longest_runtime
   ret$highest_vote_count <- highest_vote_count
   ret$highest_vote_count_tagline <- highest_vote_count_tagline
   ret$top_3_revenue_titles <- top_3_revenue_titles
-  
   return (ret)
 }
