670d877f5f1331a9498657cb0e419eba534df23c
ZhiYe <zjy@uw.edu>
2019-05-24 20:45:46 -0700

    Fixed lintr errors

--- scripts/chart_three.R
+++ scripts/chart_three.R
@@ -6,23 +6,23 @@ options(scipen = 999)
 # of movies that have been released before?
 popular_genre <- function(df) {
   most_common_genre <- df %>%
-  group_by(genres) %>% 
-  summarize(number_of_genre = n()) %>% 
-  filter(genres !="(no genres listed)") %>% 
-  arrange(number_of_genre) %>% 
-  mutate(order = factor(genres, genres)) %>% 
-  top_n(10)
+    group_by(genres) %>%
+    summarize(number_of_genre = n()) %>%
+    filter(genres != "(no genres listed)") %>%
+    arrange(number_of_genre) %>%
+    mutate(order = factor(genres, genres)) %>%
+    top_n(10)
 
   chart <- ggplot(data = most_common_genre) +
-  geom_point(
-    mapping = aes(x = number_of_genre, y = order),
-    color = "purple",
-    alpha = .7,
-    size = 6
-  ) +
-  labs(title= "Most Popular Single/Multiple Movie Genre Chart",
-       x ="Count", y = "Genre Type")
+    geom_point(
+      mapping = aes(x = number_of_genre, y = order),
+      color = "purple",
+      alpha = .7,
+      size = 6
+    ) +
+    labs(
+      title = "Most Popular Single/Multiple Movie Genre Chart",
+      x = "Count", y = "Genre Type"
+    )
   return(chart)
 }
-
-
