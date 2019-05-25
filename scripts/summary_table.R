3c927478cd6bf39f40db5e1fafa028b56544dde5
jeffrez4 <jeffrez4@uw.edu>
2019-05-24 20:18:55 -0700

    Finished Summary Table

--- scripts/summary_table.R
+++ scripts/summary_table.R
@@ -1,21 +1,18 @@
-movie_df <- read.csv("../data/movies_metadata.csv", stringsAsFactors = FALSE)
 library("dplyr") # Loads dplyr
 library("tidyr") # Loads tidyr
 library("ggplot2") # Loads ggplot2
 
-# What are the top 25 movie titles in terms of revenue?
-summary_top_25 <- movie_df %>%
-  group_by(title) %>% 
-  summarise(revenue = sum(revenue), na.rm = TRUE) %>%
-  top_n(25, wt = revenue) %>% 
-  pull(title)
 
-# What are the revenue of the top 25 movie titles?
-summary_rev <- movie_df %>%
-  group_by(title) %>%
-  summarise(revenue = sum(revenue), na.rm = TRUE) %>%
-  top_n(25, wt = revenue) %>% 
-  pull(revenue)
+# What are the top 25 movie titles in terms of revenue?
+get_summary_of_top_25 <- function(df) {
+  summary_of_top_25 <- df %>%
+  group_by(title) %>% 
+  arrange(-revenue) %>% 
+  select(title, revenue, budget, vote_average) %>% 
+  head(25)
+  return(summary_of_top_25)
+}
+
 
 
 
