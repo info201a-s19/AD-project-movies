b7ab44479c326fd74b42095db7d43440e15bd8b7
ZhiYe <zjy@uw.edu>
2019-05-24 20:50:24 -0700

    Fixed lint errors

--- scripts/summary_table.R
+++ scripts/summary_table.R
@@ -6,13 +6,9 @@ library("ggplot2") # Loads ggplot2
 # What are the top 25 movie titles in terms of revenue?
 get_summary_of_top_25 <- function(df) {
   summary_of_top_25 <- df %>%
-  group_by(title) %>% 
-  arrange(-revenue) %>% 
-  select(title, revenue, budget, vote_average) %>% 
-  head(25)
+    group_by(title) %>%
+    arrange(-revenue) %>%
+    select(title, revenue, budget, vote_average) %>%
+    head(25)
   return(summary_of_top_25)
 }
-
-
-
-
