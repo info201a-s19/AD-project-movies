65b5042057eed546bd5fb670a003145f31a434ff
jeffrez4 <jeffrez4@uw.edu>
2019-05-24 19:34:30 -0700

    Adding Chart 4

--- scripts/chart_four.R
+++ scripts/chart_four.R
@@ -2,18 +2,18 @@ library("dplyr")
 library("ggplot2")
 options(scipen = 999)
 
-movie_df <- read.csv("../data/movies_metadata.csv", stringsAsFactors = FALSE)
 # Does high run-times of movies equate to high revenue?
-
-
-test <- movie_df %>% 
+correlation <- function(df) {
+  run_vs_rev <- df %>% 
   filter(runtime != 0) %>% 
   filter(revenue != 0) %>% 
   select(runtime, revenue)
 
-ggplot(data = test) +
+  scatterplot <- ggplot(data = run_vs_rev) +
   geom_point(mapping = aes(x = runtime, y = revenue), alpha = (1/10)) +
-  scale_x_continuous(breaks=seq(1900,2020,5)) +
-  scale_y_continuous(breaks=seq(0, max(test$revenue), 500000000)) +
+  scale_x_continuous(breaks=seq(0,max(run_vs_rev$runtime),20)) +
+  scale_y_continuous(breaks=seq(0, max(run_vs_rev$revenue), 500000000)) +
   labs(title= "Scatter Plot of Movie Run Time and Revenue",
-       x ="(minutes) Movie Run Time", y = "($) Revenue")
+       x ="Run Time in Minutes", y = "($) Revenue")
+  return(scatterplot)
+}
