988e396f2cf314dc9155301002c2cb78baf99e16
ZhiYe <zjy@uw.edu>
2019-06-04 10:57:30 -0700

    Need changes

--- app_ui.R
+++ app_ui.R
@@ -33,8 +33,55 @@ page_one <- tabPanel(
 )
 
 
+# Create x and y variables for page two
+yxaxis_var_one <- list(
+  "Budget" = "budget",
+  "Revenue" = "revenue",
+  "Runtime" = "runtime",
+  "Votes Count" = "vote_count",
+  "Vote Average" = "vote_average"
+)
+
+# Define the first page content; uses tabPanel, sidebarLayout, 
+# sidebarPanel, selectInput, mainPanel, etc. 
+page_two <- tabPanel(
+  "Zhi Ye", # label for the tab in the navbar
+  titlePanel("Zhi Ye"), # show with a displayed title
+  # This content uses a sidebar layout
+  sidebarLayout(
+    sidebarPanel(
+      selectInput(
+        "x_var_one",
+        label = "X Variable",
+        choices = yxaxis_var_one,
+        selected = "vote_average"
+      ),
+      selectInput(
+        "y_var_one",
+        label = "Y Variable",
+        choices = yxaxis_var_one,
+        selected = "vote_count"
+      ),
+      selectInput(
+        "color_one",
+        label = "Color",
+        choices = list("Blue" = "blue", "Green" = "green", "Red" = "red")
+      ),
+      size_input <- sliderInput(
+        "size_one",
+        label = "Size of point", min = 1, max = 10, value = 2
+      )
+    ),
+    mainPanel(
+      plotOutput("scatter")
+    )
+  )
+)
+
+
 # Pass each page to a multi-page layout (`navbarPage`)
 ui <- navbarPage(
   "My Application", # application title
-  page_one
+  page_one,
+  page_two
 )
