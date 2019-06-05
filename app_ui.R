f678640d6d4be7626db20ee282ad0783bb7ccb5b
Jody Wong <jodyw22@uw.edu>
2019-06-04 23:04:36 -0700

    edited summary page information

--- app_ui.R
+++ app_ui.R
@@ -132,9 +132,36 @@ page_three <- tabPanel(
 summary_page <- tabPanel(
   "Summary",
   mainPanel(
-    tags$h1("Conclusion of our Data"),
-    tags$p(" Our conclusion with this ejsokfjsdlkfsdkfj .....
-           ")
+    h1("Summary"),
+    h2("Budget/Renevues Analysis"),
+    p("Throughout the assignment, our group noticed some fascinating things
+    regarding the movies dataset that we have been working with. From one of
+    our main takeaways, there was an interesting increase within movie budgets
+    and revenues that have increased over the time in years. This was also
+    prelevant within the first interactive chart, which shows the actual
+    increase of the budgets and revenues over time. "),
+    h2("Revenue, Budget, and Popularity Conclusions"),
+    p("Our second conclusion consists of revenue, budget, and popularity and 
+      the correlation that is connected with these three variables. From the
+      aspect that revenue and popularity have a positive correlation, and also
+      so does the budget and popularity. All of these equates to the fact that
+      a more money that is spent or made upon a movie, would also have a
+      higher popularity, which gains more popularity."),
+    h2("Top 10 Movies Takeaway"),
+    p("Within the top 10 movies that were highly rated, it came to our
+    relization that majority of the movies were made fairly recently (within 
+    the past 20 years). Although the whole movie dataset actually contained
+    movies from 1900s until the 2000s from the aggregated database, we can
+    implicate that the modernization has increased over time as well as 
+    the technological advances that can track the amount of votes within the
+    movie industry. Also, the improvement of the movie dataset gained quality
+    over time, as the most recent data has a better record of voting."),
+    h2("Conclusion"),
+    p("Within the whole movie dataset, our team learned a lot about movies and
+      how they impact society. From the financial aspects such as budgeting, to
+      the impact of popularity and vote counts,movies contain a vital role
+      within social media and society. This dataset helped us understand
+      the true importance of movies and how their data can affect society.")
   )
 )
 
